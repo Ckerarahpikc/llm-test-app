680a47b276e846c8657d99da47024c47f372cfc1
Ckerarahpikc <werrrom@gmail.com>
2026-05-29 17:53:27 +0300

    feat: implementing chat functionality & fixing some mistakes

--- llm-backend/index.js
+++ llm-backend/index.js
@@ -1,27 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { GoogleGenAI } = require("@google/genai");
 require("dotenv").config({ path: "./.env" });
 
+// APPLICATION SETUP
 const app = express();
-
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+// INITIALIZE UNIFIED GEMINI SDK
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 app.post("/api/chat", async (req, res) => {
   try {
     const { message } = req.body;
 
-    const fakeMessage = "New messsage jjs fo rteslkajdfj";
+    if (!message) {
+      return res.status(400).json({ status: "Message is required" });
+    }
 
-    res.status(200).json({ reply: fakeMessage });
+    // generate the content using sdk syntax
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: message,
+    });
+
+    res.json({ reply: response.text });
   } catch (err) {
     res.status(500).json({ status: "Something went wrong." });
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
   console.log(`> ✅ App running on http://localhost:${PORT}`);
 });
