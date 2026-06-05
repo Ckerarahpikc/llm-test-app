59bae409e7a10a1233707126bf5bc79e38b5e951
Ckerarahpikc <werrrom@gmail.com>
2026-06-05 12:37:33 +0300

    feat: updating F to use 'useChat' from sdk react
    usechat will help to manage and output streams that are comming from
    backend

--- llm-frontend/src/App.jsx
+++ llm-frontend/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState, useRef, useEffect } from "react";
+import { HiMiniPencilSquare } from "react-icons/hi2";
+import { DefaultChatTransport } from "ai";
+
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import { HiMiniPencilSquare } from "react-icons/hi2";
+
+import { useChat } from "@ai-sdk/react";
 
 function App() {
-  // const [isEditing, setIsEditing] = useState(false);
-  const [input, setInput] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [messages, setMessages] = useState([]);
   const messagesEndRef = useRef(null);
+  const [input, setInput] = useState("");
+  const { messages, sendMessage, status } = useChat({
+    transport: new DefaultChatTransport({
+      api: "http://localhost:2000/api/chat",
+    }),
+  });
+  const isLoading = status === "submitted";
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      sendMessage({ text: input });
+      setInput("");
+    }
+  };
 
   useEffect(() => {
     if (messagesEndRef.current) {
@@ -17,172 +32,145 @@ function App() {
     }
   }, [messages]);
 
-  const sendMessage = async () => {
-    if (!input.trim() || isLoading) return;
-
-    // add user message to the ui
-    const userMessage = { role: "user", text: input };
-    const typingIndicator = { role: "typing", id: "ai_typing_indicator" };
-    setMessages((prev) => [...prev, userMessage, typingIndicator]);
-    setInput("");
-    setIsLoading(true);
-
-    try {
-      const res = await fetch("http://localhost:2000/api/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message: userMessage.text }),
-      });
-
-      const data = await res.json();
-
-      if (res.ok) {
-        setMessages((prev) => {
-          const updatedMessages = prev.filter(
-            (msg) => msg.id !== "ai_typing_indicator",
-          );
-          return [...updatedMessages, { role: "ai", text: data.reply }];
-        });
-      } else {
-        console.log("Server error:", data.error);
-        // still removing the typing indicator
-        setMessages((prev) =>
-          prev.filter((msg) => msg.id !== "ai_typing_indicator"),
-        );
-      }
-    } catch (err) {
-      console.error("Error connecting to backend:", err);
-      // still removing the typing indicator
-      setMessages((prev) =>
-        prev.filter((msg) => msg.id !== "ai_typing_indicator"),
-      );
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  // const editMessage = async () => {
-  //
-  // };
-
   return (
     <div
       style={{
-        padding: "20px",
-        maxWidth: "1000px",
-        minWidth: "800px",
+        width: "100%",
+        height: "100vh",
         margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
       }}
     >
       <div
         style={{
-          height: "400px",
-          overflowY: "auto",
-          border: "1px solid #ccc",
+          width: "100%",
+          overflowY: "hidden",
           padding: "10px",
           marginBottom: "10px",
           display: "flex",
           flexDirection: "column",
         }}
       >
-        {messages.map((msg, index) => (
-          <div
-            key={msg.id || index}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: msg.role === "user" ? "flex-end" : "flex-start",
-              margin: "16px 0",
-              width: "100%",
-            }}
-          >
+        {messages.map((msg) => {
+          let textMessage = msg.parts
+            ?.filter((part) => part.type === "text")
+            .map((part) => part.text)
+            .join("");
+
+          return (
             <div
+              key={msg.id}
               style={{
-                backgroundColor: msg.role === "user" ? "#3a393c" : "#242325",
-                padding: "10px 14px",
-                borderRadius:
-                  msg.role === "user"
-                    ? "16px 16px 4px 16px"
-                    : "16px 16px 16px 4px",
-                maxWidth: "75%",
-                wordBreak: "break-word",
-                color: msg.role === "typing" ? "#999" : "inherit",
-                fontStyle: msg.role === "typing" ? "italic" : "normal",
-                boxShadow: "0 1px 2px rgba(0,0,0,0.1)",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: msg.role === "user" ? "flex-end" : "flex-start",
+                margin: "16px 0",
+                width: "100%",
               }}
             >
-              {msg.role === "typing" ? (
-                <em
-                  style={{ display: "flex", alignItems: "center", gap: "8px" }}
-                >
-                  Gemini is typing...
-                </em>
-              ) : (
+              <div
+                style={{
+                  backgroundColor: msg.role === "user" ? "#3a393c" : "#242325",
+                  padding: "10px 14px",
+                  borderRadius:
+                    msg.role === "user"
+                      ? "16px 16px 4px 16px"
+                      : "16px 16px 16px 4px",
+                  maxWidth: "75%",
+                  wordBreak: "break-word",
+                  textAlign: msg.role === "user" ? "right" : "left",
+                  color: msg.role === "user" ? "#999" : "inherit",
+                  fontStyle: msg.role === "user" ? "italic" : "normal",
+                  boxShadow: "0 1px 2px rgba(0,0,0,0.1)",
+                }}
+              >
                 <ReactMarkdown
                   remarkPlugins={[remarkGfm]}
                   rehypePlugins={[rehypeRaw]}
                 >
-                  {msg.text}
+                  {textMessage}
                 </ReactMarkdown>
+              </div>
+
+              {msg.role === "user" && (
+                <div
+                  style={{
+                    marginTop: "4px",
+                    marginRight: "4px",
+                    color: "#aaa",
+                    cursor: "pointer",
+                    fontSize: "1.1rem",
+                    display: "flex",
+                    alignItems: "center",
+                    opacity: 0.7,
+                  }}
+                  title="Edit message"
+                >
+                  <HiMiniPencilSquare />
+                </div>
               )}
             </div>
+          );
+        })}
 
-            {msg.role === "user" && (
-              <div
-                // onClick={editMessage}
-                style={{
-                  marginTop: "4px",
-                  marginRight: "4px",
-                  color: "#aaa",
-                  cursor: "pointer",
-                  fontSize: "1.1rem",
-                  display: "flex",
-                  alignItems: "center",
-                  opacity: 0.7,
-                }}
-                title="Edit message"
-              >
-                <HiMiniPencilSquare />
-              </div>
-            )}
+        {isLoading && (
+          <div
+            style={{
+              fontStyle: "italic",
+              color: "#999",
+              margin: "10px 0",
+              alignSelf: "flex-start",
+            }}
+          >
+            Gemini is preparing a response...
           </div>
-        ))}
+        )}
+
         {/* this div will be scrolled into view */}
         <div ref={messagesEndRef} />
       </div>
 
-      <div style={{ display: "flex", gap: "10px" }}>
+      <form
+        onSubmit={onSubmit}
+        style={{
+          position: "relative",
+          display: "flex",
+          gap: "10px",
+          minWidth: "900px",
+          bottom: "50px",
+        }}
+      >
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
           placeholder="Ask Gemini something..."
           style={{
             flexGrow: 1,
-            padding: "10px",
+            padding: "14px",
             border: "1px solid #ccc",
-            borderRadius: "5px",
+            borderRadius: "50px",
           }}
           disabled={isLoading}
         />
         <button
-          onClick={sendMessage}
+          type="submit"
           disabled={isLoading}
           style={{
             padding: "10px 15px",
             backgroundColor: "#007bff",
             color: "white",
             border: "none",
-            borderRadius: "5px",
+            borderRadius: "50px",
             cursor: isLoading ? "not-allowed" : "pointer",
           }}
         >
           {isLoading ? "Sending..." : "Send"}
         </button>
-      </div>
+      </form>
     </div>
   );
 }
